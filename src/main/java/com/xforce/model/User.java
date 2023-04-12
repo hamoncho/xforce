821e83a790a4e5155635ef967d243a60a71c38db
hamoncho <hamsvidal@gmail.com>
2023-04-12 15:35:22 -0600

    la creacion de la clase User

--- src/main/java/com/xforce/model/User.java
+++ src/main/java/com/xforce/model/User.java
@@ -8,12 +8,70 @@ package com.xforce.model;
  *
  * @author hamoncho
  */
-public record User(
-        int id_user,
-        String userName,
-        String phone,
-        String password,
-        String email,
-        String name,
-        String lastNames,
-        UserType userType) {}
+public class User {
+
+    private int id_Usuario;
+    private String username;
+    private String email;
+    private String telefono;
+    private String contrasenna;
+    private int tipo_Usuario;
+    private String lastNames;
+
+    public int getId_Usuario() {
+        return id_Usuario;
+    }
+
+    public void setId_Usuario(int id_Usuario) {
+        this.id_Usuario = id_Usuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasenna() {
+        return contrasenna;
+    }
+
+    public void setContrasenna(String contrasenna) {
+        this.contrasenna = contrasenna;
+    }
+
+    public int getTipo_Usuario() {
+        return tipo_Usuario;
+    }
+
+    public void setTipo_Usuario(int tipo_Usuario) {
+        this.tipo_Usuario = tipo_Usuario;
+    }
+
+    public String getLastNames() {
+        return lastNames;
+    }
+
+    public void setLastNames(String lastNames) {
+        this.lastNames = lastNames;
+    }
+
+}
