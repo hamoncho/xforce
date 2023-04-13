cf9e00ff35a06a17087bd9b6128fb637e13b5724
hamoncho <hamsvidal@gmail.com>
2023-04-12 18:05:30 -0600

    add class Cliente

---
+++ src/main/java/com/xforce/model/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xforce.model;
+
+/**
+ *
+ * @author hamoncho
+ */
+
+public class Cliente extends User {
+    
+    private String nombre;
+    private String apellido;
+    private String num_targeta;
+
+}
