8d2aa1b6cce2f002571e110930a358e27a6af9de
hamoncho <hamsvidal@gmail.com>
2023-04-14 14:22:04 -0600

    agrega metodos onMouseCliked a los itemsArea

--- src/main/java/com/xforce/controller/AreasController.java
+++ src/main/java/com/xforce/controller/AreasController.java
@@ -6,7 +6,10 @@ package com.xforce.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -15,12 +18,45 @@ import javafx.fxml.Initializable;
  */
 public class AreasController implements Initializable {
 
+    @FXML
+    private VBox VBoxCardiovascularArea;
+    @FXML
+    private VBox VBoxFunctionalFitnessArea;
+    @FXML
+    private VBox VBoxStretchingAndMobilityArea;
+    @FXML
+    private VBox VBoxPersonalTrainingCorners;
+    @FXML
+    private VBox VBoxFreeWeightsArea;
+
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void OnMouseCardiovascularArea(MouseEvent event) {
+    }
+
+    @FXML
+    private void OnMouseClickedFunctionalFitnessArea(MouseEvent event) {
+    }
+
+    @FXML
+    private void OnMouseClikedFreeWeightsArea(MouseEvent event) {
+    }
+
+    @FXML
+    private void OnMouseClikedStretchingAndMobilityArea(MouseEvent event) {
+    }
+
+    @FXML
+    private void OnMouseClikedPersonalTrainingCorners(MouseEvent event) {
+    }
     
 }
